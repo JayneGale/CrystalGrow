7b66dbced06901139337bfb22f0e7d68aa649164
Jayne Gale <jaynegale@gmail.com>
2019-05-31 15:31:50 +1200

    Presented

---
+++ CrystalGrow/Assets/Animation/Scripts/ClusterGrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterGrow : MonoBehaviour {
+
+    public GameObject crystal;
+    public string triggerName;
+    public KeyCode keyToPress;
+    Animator crystalAnimator;
+    Renderer crystalRenderer;
+
+    // Use this for initialization
+    void Start () {
+        crystalAnimator = gameObject.GetComponent<Animator>();
+        crystalRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            Debug.Log(" keyToPress is pressed " + keyToPress);
+            crystalRenderer.enabled = true;
+            crystalAnimator.SetTrigger(triggerName);
+        }
+	}
+}
---
+++ CrystalGrow/Assets/Animation/Scripts/StartGrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGrow : MonoBehaviour {
+
+    public GameObject crystal;
+    public string triggerName;
+    public KeyCode keyToPress;
+    Animator crystalAnimator;
+    Renderer crystalRenderer;
+
+    void Start () {
+        crystalAnimator = gameObject.GetComponent<Animator>();
+        crystalRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+            if (Input.GetKeyDown(keyToPress))
+        {
+            Debug.Log(" keyToPress is pressed " + keyToPress);
+            crystalRenderer.enabled = true;
+            crystalAnimator.SetTrigger(triggerName);
+        }
+	}
+}
